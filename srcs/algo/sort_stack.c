3a25769c818d7b138589551f3cd56419d72583b3
Julien Banchongphanith <jbanchon@made-f0Ar9s3.clusters.42paris.fr>
2024-11-05 16:12:50 +0100

    last

--- srcs/algo/sort_stack.c
+++ srcs/algo/sort_stack.c
@@ -6,8 +6,33 @@
 /*   By: jbanchon <jbanchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:24:43 by jbanchon          #+#    #+#             */
-/*   Updated: 2024/11/05 15:24:54 by jbanchon         ###   ########.fr       */
+/*   Updated: 2024/11/05 15:48:58 by jbanchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/push_swap.h"
+
+void	sort_five(t_stack **a, t_stack **b)
+{
+
+}
+
+void	sort_four(t_stack **a, t_stack **b)
+{
+
+}
+
+void	sort_three(t_stack **a)
+{
+	
+}
+
+int	stack_sorted(t_stack **stack)
+{
+
+}
+
+void	sort_stack(t_stack **a, t_stack **b)
+{
+
+}
